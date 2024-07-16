d0b4433218c8542cc848a9fa51d7290716dac76e
iculmination <ovsiichuk.bohdan@gmail.com>
2024-07-16 22:59:04 +0300

    slider

--- src/components/blocks/welcome-block.tsx
+++ src/components/blocks/welcome-block.tsx
@@ -1,40 +1,19 @@
-"use client";
-
-import Image from "next/image";
-
-import { Navigation, Pagination, Scrollbar, A11y, Zoom } from "swiper/modules";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/swiper-bundle.css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
+import { Button } from "../ui/button";
+import SwiperMain from "../ui/swiper";
 
 const WelcomeBlock = () => {
-  const images: string[] = [];
   return (
-    <section>
-      <Swiper
-        modules={[Navigation, Pagination]}
-        slidesPerView={1}
-        navigation
-        pagination={{ clickable: true }}
-        className="border border-zinc-300 rounded-t-lg bg-zinc-100 flex items-center justify-center self-center"
-      >
-        {images.map((image) => (
-          <SwiperSlide
-            key={image}
-            className="flex items-center justify-center self-center"
-          >
-            <Image
-              src={image}
-              alt={image}
-              key={image}
-              height={500}
-              width={500}
-              className="object-contain self-center"
-            />
-          </SwiperSlide>
-        ))}
-      </Swiper>
+    <section className="relative flex items-center justify-center">
+      <div className="absolute z-10 text-white flex flex-col justify-center space-y-4 ">
+        <h2 className="text-center drop-shadow-lg text-6xl font-bold">
+          <p>Ласкаво просимо</p>
+          <p>
+            до Volyn<span className="text-pink-200">pack</span>
+          </p>
+        </h2>
+        <Button className="mx-auto text-md">Про нас</Button>
+      </div>
+      <SwiperMain />
     </section>
   );
 };
---
+++ src/components/ui/swiper.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import Image from "next/image";
+
+import { Autoplay, Navigation, Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "../../app/globals.css";
+
+const SwiperMain = () => {
+  const images: string[] = [
+    "/bags-wide.png",
+    "/bags-wide.png",
+    "/bags-wide.png",
+    "/bags-wide.png",
+  ];
+  return (
+    <Swiper
+      modules={[Navigation, Pagination, Autoplay]}
+      slidesPerView={1}
+      pagination={{ clickable: true }}
+      autoplay
+      navigation
+      className="bg-black/50 custom-swiper"
+    >
+      {images.map((image) => (
+        <SwiperSlide key={image}>
+          <Image
+            src={image}
+            alt={image}
+            key={image}
+            height={1000}
+            width={1000}
+            className="mx-auto w-full bg-contain"
+          />
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
+
+export default SwiperMain;
