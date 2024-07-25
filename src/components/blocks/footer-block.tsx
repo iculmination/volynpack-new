3ae18397ae9297fe5e96dade7dbed8df2a6900e4
iculmination <ovsiichuk.bohdan@gmail.com>
2024-07-26 00:25:12 +0300

    footer

--- src/components/blocks/footer-block.tsx
+++ src/components/blocks/footer-block.tsx
@@ -1,12 +1,36 @@
+import { Mail, MapPin, Phone } from "lucide-react";
+
 const Header = () => {
   return (
-    <footer className="w-full py-20 bg-black">
-      <div className="container">
+    <footer className="w-full py-14 bg-black">
+      <div className="container flex justify-between text-gray-400">
         <h3 className="">
           <a href="/" className="text-white font-bold text-4xl">
             Volyn<span className="text-pink-600">pack</span>
           </a>
         </h3>
+        <nav className="flex space-x-10">
+          <a
+            className="flex items-center hover:text-white transition duration-200"
+            href="mailto:example@gmail.com"
+          >
+            <Mail className="text-pink-600 mr-2" />
+            example@gmail.com
+          </a>
+          <a
+            className="flex items-center hover:text-white transition duration-200"
+            href="tel:1234567890"
+          >
+            <Phone className="text-pink-600 mr-2" /> +1-234-56-78-90
+          </a>
+          <a
+            className="flex items-center hover:text-white transition duration-200"
+            href="#"
+          >
+            <MapPin className="text-pink-600 mr-2" /> 09100, Street, City,
+            Country
+          </a>
+        </nav>
       </div>
     </footer>
   );
